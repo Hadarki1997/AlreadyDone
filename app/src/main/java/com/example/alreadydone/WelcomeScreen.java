713db199adc6475fbd01d450a8513944e05237e0
hadar givoni <hadarki1996@gmail.com>
2024-05-18 01:47:12 +0300

    commit

--- app/src/main/java/com/example/alreadydone/WelcomeScreen.java
+++ app/src/main/java/com/example/alreadydone/WelcomeScreen.java
@@ -1,6 +1,9 @@
 package com.example.alreadydone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeScreen extends AppCompatActivity {
@@ -8,5 +11,11 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        Button getStartedButton = findViewById(R.id.button_get_started);
+        getStartedButton.setOnClickListener(v -> {
+            Intent intent = new Intent(WelcomeScreen.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
