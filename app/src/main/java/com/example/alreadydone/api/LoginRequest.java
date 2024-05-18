04afd99b22d7cf8f0902fad59652d838a9a9cd8b
hadar givoni <hadarki1996@gmail.com>
2024-05-19 01:37:48 +0300

    commit

--- app/src/main/java/com/example/alreadydone/api/LoginRequest.java
+++ app/src/main/java/com/example/alreadydone/api/LoginRequest.java
@@ -9,5 +9,21 @@ public class LoginRequest {
         this.password = password;
     }
 
-    // Getters and Setters
+    // Getters
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- app/src/main/java/com/example/alreadydone/api/RegisterRequest.java
+++ app/src/main/java/com/example/alreadydone/api/RegisterRequest.java
@@ -11,5 +11,29 @@ public class RegisterRequest {
         this.password = password;
     }
 
-    // Getters and Setters
+    // Getters
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
